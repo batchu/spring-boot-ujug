247c97c6c266b05bc4c1b133630ffafb8bd2b548
Prashanth Batchu <prashanth@batchu.net>
2015-03-14 15:32:03 -0600

    Added working unit test for the hello world endpoint

--- Hello World/src/test/java/demo/DemoApplicationTests.java
+++ Hello World/src/test/java/demo/DemoApplicationTests.java
@@ -1,23 +1,38 @@
 package demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 public class DemoApplicationTests {
 	
-	final String BASE_URL = "http://localhost:8080/";
+	    private MockMvc mockMvc;
 
+	    @Before
+	    public void setup() {
+	        this.mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorld()).build();
+	    }
+	    
 	 
 	@Test
-	public void testSayHelloWorld(){
-		RestTemplate rest = new TestRestTemplate();
-		String resp = rest.getForObject(BASE_URL, String.class);
+	public void testSayHelloWorld() throws Exception{
+		
+		 this.mockMvc.perform(get("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+         .andExpect(status().isOk())
+         .andExpect(content().contentType("application/json"));
+         
 	}
 
 	@Test
