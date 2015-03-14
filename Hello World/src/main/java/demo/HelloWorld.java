695246305ad50edd18e2b576078d00b87927710d
Prashanth Batchu <prashanth@batchu.net>
2015-03-14 14:25:06 -0600

    Removed duplicate main class, in the process of adding a Controller Test.

--- Hello World/src/main/java/demo/HelloWorld.java
+++ Hello World/src/main/java/demo/HelloWorld.java
@@ -1,22 +1,18 @@
 package demo;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloWorld {
 
+	 
     @RequestMapping("/")
-    String home() {
+    String sayHelloWorld() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloWorld.class, args);
     }
 
 }
--- Hello World/src/test/java/demo/DemoApplicationTests.java
+++ Hello World/src/test/java/demo/DemoApplicationTests.java
@@ -3,11 +3,22 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 public class DemoApplicationTests {
+	
+	final String BASE_URL = "http://localhost:8080/";
+
+	 
+	@Test
+	public void testSayHelloWorld(){
+		RestTemplate rest = new TestRestTemplate();
+		String resp = rest.getForObject(BASE_URL, String.class);
+	}
 
 	@Test
 	public void contextLoads() {
